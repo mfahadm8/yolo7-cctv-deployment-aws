08faeb887bd435783b2a6c4c26139289997dec41
Ubuntu <ubuntu@ip-172-31-12-52.ec2.internal>
2023-08-24 13:27:59 +0000

    bug fixes

--- code/inference.py
+++ code/inference.py
@@ -1,38 +1,43 @@
 import argparse
 import time
 from pathlib import Path
+import json
 
 import cv2
 import torch
-import torch.backends.cudnn as cudnn
-import numpy as np
-from pandas import DataFrame
 import json
-
 from models.experimental import attempt_load
-from utils.datasets import LoadStreams, LoadImages
-from utils.general import check_img_size, check_requirements, check_imshow, non_max_suppression, apply_classifier, \
-    scale_coords, xyxy2xywh, strip_optimizer, set_logging, increment_path
-from utils.plots import plot_one_box
-from utils.torch_utils import select_device, load_classifier, time_synchronized, TracedModel
-
-from tracker.byte_tracker import BYTETracker
 import os
 import io
-
+from pandas import DataFrame
 import json
 import logging
 import tempfile
-
+import boto3
 import cv2
 import torch
 import torchvision.transforms as transforms
-
+import traceback
 # This code will be loaded on each worker separately..
+
+from numpy import random
+import numpy as np
+from random import randint
+from utils.datasets import LoadImages
+from utils.general import check_img_size, check_requirements, check_imshow, non_max_suppression, apply_classifier, \
+    scale_coords, xyxy2xywh, strip_optimizer, set_logging, increment_path
+from utils.torch_utils import time_synchronized, TracedModel
+from utils.plots import plot_one_box
+from tracker.byte_tracker import BYTETracker
+stride = None
+imgsz=1024
+s3 = boto3.resource('s3')
+
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
 def model_fn(model_dir):
+    global stride
     device = get_device()
     logger.info(">>> Device is '%s'.." % device)
     model = attempt_load(model_dir + '/best.pt', map_location=torch.device(device))
@@ -40,105 +45,67 @@ def model_fn(model_dir):
     logger.info(type(model))
     logger.info(">>> Model loaded!..")
     logger.info(model)
+    stride = int(model.stride.max())
+    model = TracedModel(model, device, imgsz)
     return model
 
+
+def get_s3_bucket_and_key(s3_location_uri):
+    s3_path_without_prefix = s3_location_uri["s3_input_path"][len("s3://"):]
+    return s3_path_without_prefix.split('/', 1) #bucket_name, key
+    
 def transform_fn(model, request_body, content_type, accept):
-    interval = int(os.environ.get('FRAME_INTERVAL', 30))
-    frame_width = int(os.environ.get('FRAME_WIDTH', 1024))
-    frame_height = int(os.environ.get('FRAME_HEIGHT', 1024))
-    batch_size = int(os.environ.get('BATCH_SIZE', 24))
+    try:
+        input_data = json.loads(request_body)
+        input_location=input_data["input_location"]
+        output_label_location=input_data["output_label_location"]
+        output_video_location=input_data["output_video_location"]
+        
+        logger.info(">>> input_data"+input_location+","+output_label_location+","+output_video_location)
+        # Split the path into bucket name and key
+        bucket_name, key = get_s3_bucket_and_key(input_location)
+        base_filename = os.path.basename(key)
+        # Create a temporary file in the system's temporary directory
+        temp_dir = tempfile.gettempdir()
+        local_filename = os.path.join(temp_dir, base_filename)
 
-    f = io.BytesIO(request_body)
-    tfile = tempfile.NamedTemporaryFile(delete=False)
-    tfile.write(f.read())
+        # Download the S3 file
 
-    all_predictions = []
+        my_bucket = s3.Bucket(bucket_name)
+        my_bucket.download_file(key, local_filename)
 
-    for batch_frames in batch_generator(tfile, frame_width, frame_height, interval, batch_size):
-        batch_inputs = preprocess(batch_frames)  # returns 4D tensor
-        batch_outputs = predict(batch_inputs, model)
-        logger.info(">>> Length of batch predictions: %d" % len(batch_outputs))
-        batch_predictions = postprocess(batch_outputs)
-        all_predictions.extend(batch_predictions)
-    
-    logger.info(">>> Length of final predictions: %d" % len(all_predictions))
-    return json.dumps(all_predictions)
+        ouput_path= detect(local_filename,model,output_label_location,output_video_location)
+        return json.dumps({"output_path":""})
 
-def preprocess(inputs, preprocessor=transforms.ToTensor()):
-    outputs = torch.stack([preprocessor(frame) for frame in inputs])
-    return outputs
-    
-def predict(inputs, model):
-    logger.info(">>> Invoking model!..")
+    except Exception as e:
+        logger.error(traceback.format_exc())
+        return json.dumps({"Error":traceback.format_exc()})
 
-    with torch.no_grad():
-        device = get_device()
-        model = model.to(device)
-        input_data = inputs.to(device)
-        model.eval()
-        outputs = model(input_data)
-
-    return outputs
-
-def postprocess(inputs):
-    outputs = []
-    for inp in inputs:
-        outputs.append({
-            'boxes': inp['boxes'].detach().cpu().numpy().tolist(),
-            'labels': inp['labels'].detach().cpu().numpy().tolist(),
-            'scores': inp['scores'].detach().cpu().numpy().tolist()
-        })
-    return outputs
 
 def get_device():
     device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
     return device
 
-def batch_generator(tfile, frame_width, frame_height, interval, batch_size):
-    cap = cv2.VideoCapture(tfile.name)
-    frame_index = 0
-    frame_buffer = []
 
-    while cap.isOpened():
+def detect(video_file,model,output_label_location,output_video_location):
+    global stride,imgsz
+    augment=False
+    agnostic_nms=False
+    classes=None
+    iou_thres=0.45
+    conf_thres=0.25
+    save_conf=True
+    track_thresh = 0.6
+    track_buffer = 30
+    match_thresh = 0.9
+    mot20=False
+    save_img=True
 
-        success, frame = cap.read()
+    save_with_object_id=True
+    save_txt=True
+    save_bbox_dim=True
 
-        if not success:
-            cap.release()
-            if frame_buffer:
-                yield frame_buffer
-            return
-
-        if frame_index % interval == 0:
-            frame_resized = cv2.resize(frame, (frame_width, frame_height), interpolation=cv2.INTER_AREA)
-            frame_buffer.append(frame_resized)
-
-        if len(frame_buffer) == batch_size:
-            yield frame_buffer
-            frame_buffer.clear()
-
-        frame_index += 1
-    else:
-        raise Exception("Failed to open video '%s'!.." % tfile.name)
-
-
-def detect(save_img=False):
-    source, weights, view_img, save_txt, imgsz, trace = opt.source, opt.weights, opt.view_img, opt.save_txt, opt.img_size, not opt.no_trace
-    save_img = not opt.nosave and not source.endswith('.txt')  # save inference images
-    webcam = source.isnumeric() or source.endswith('.txt') or source.lower().startswith(
-        ('rtsp://', 'rtmp://', 'http://', 'https://'))
-
-    # Directories
-    save_dir = Path(increment_path(Path(opt.project) / opt.name, exist_ok=opt.exist_ok))  # increment run
-    (save_dir / 'labels' if save_txt else save_dir).mkdir(parents=True, exist_ok=True)  # make dir
-
-    # Initialize
-    set_logging()
-    device = select_device(opt.device)
-    half = device.type != 'cpu'  # half precision only supported on CUDA
-
-    # Initialize Tracker
-    tracker = BYTETracker(opt) # track_thresh, match_thresh, mot20
+    tracker = BYTETracker(track_thresh,track_buffer,mot20,match_thresh) # track_thresh, match_thresh, mot20
     track_results = {   'Frame': [],
                         'top':[],
                         'left':[],
@@ -146,33 +113,37 @@ def detect(save_img=False):
                         'height':[],
                         'track_id':[]
                     }
-        
-    # Load model
-    model = attempt_load(weights, map_location=device)  # load FP32 model
-    stride = int(model.stride.max())  # model stride
+    #......................... 
+    save_dir = Path(increment_path(Path("runs/detect") / "object_detection", exist_ok=True))  # increment run
+    (save_dir / 'labels' if save_txt else save_dir).mkdir(parents=True, exist_ok=True)  # make dir
+
+    
+    #........Rand Color for every trk.......
+    rand_color_list = []
+    amount_rand_color_prime = 5003 # prime number
+    for i in range(0,amount_rand_color_prime):
+        r = randint(0, 255)
+        g = randint(0, 255)
+        b = randint(0, 255)
+        rand_color = (r, g, b)
+        rand_color_list.append(rand_color)
+    #......................................
+   
     imgsz = check_img_size(imgsz, s=stride)  # check img_size
 
-    if trace:
-        model = TracedModel(model, device, opt.img_size)
+    device = get_device()
 
-    if half:
-        model.half()  # to FP16
-
-     # Set Dataloader
+    # Set Dataloader
     vid_path, vid_writer = None, None
-    if webcam:
-        view_img = check_imshow()
-        cudnn.benchmark = True  # set True to speed up constant image size inference
-        dataset = LoadStreams(source, img_size=imgsz, stride=stride)
-    else:
-        dataset = LoadImages(source, img_size=imgsz, stride=stride)
 
+    dataset = LoadImages(video_file, img_size=imgsz, stride=stride)
+    half = device!= 'cpu'
     # Get names and colors
     names = model.module.names if hasattr(model, 'module') else model.names
-    colors = [[np.random.randint(0, 255) for _ in range(3)] for _ in names]
+    colors = [[random.randint(0, 255) for _ in range(3)] for _ in names]
 
     # Run inference
-    if device.type != 'cpu':
+    if device != 'cpu':
         model(torch.zeros(1, 3, imgsz, imgsz).to(device).type_as(next(model.parameters())))  # run once
     old_img_w = old_img_h = imgsz
     old_img_b = 1
@@ -180,6 +151,7 @@ def detect(save_img=False):
     frame_id = 0
 
     t0 = time.time()
+    
     for path, img, im0s, vid_cap in dataset:
         frame_id += 1
         img = torch.from_numpy(img).to(device)
@@ -189,29 +161,26 @@ def detect(save_img=False):
             img = img.unsqueeze(0)
 
         # Warmup
-        if device.type != 'cpu' and (old_img_b != img.shape[0] or old_img_h != img.shape[2] or old_img_w != img.shape[3]):
+        if device != 'cpu' and (old_img_b != img.shape[0] or old_img_h != img.shape[2] or old_img_w != img.shape[3]):
             old_img_b = img.shape[0]
             old_img_h = img.shape[2]
             old_img_w = img.shape[3]
             for i in range(3):
-                model(img, augment=opt.augment)[0]
+                model(img, augment=augment)[0]
 
         # Inference
         t1 = time_synchronized()
         with torch.no_grad():   # Calculating gradients would cause a GPU memory leak
-            pred = model(img, augment=opt.augment)[0]
+            pred = model(img, augment=augment)[0]
         t2 = time_synchronized()
 
         # Apply NMS
-        pred = non_max_suppression(pred, opt.conf_thres, opt.iou_thres, classes=opt.classes, agnostic=opt.agnostic_nms)
+        pred = non_max_suppression(pred, conf_thres, iou_thres, classes=classes, agnostic=agnostic_nms)
         t3 = time_synchronized()
 
         # Process detections
         for i, det in enumerate(pred):  # detections per image
-            if webcam:  # batch_size >= 1
-                p, s, im0, frame = path[i], '%g: ' % i, im0s[i].copy(), dataset.count
-            else:
-                p, s, im0, frame = path, '', im0s, getattr(dataset, 'frame', 0)
+            p, s, im0, frame = path, '', im0s, getattr(dataset, 'frame', 0)
 
             p = Path(p)  # to Path
             save_path = str(save_dir / p.name)  # img.jpg
@@ -231,11 +200,11 @@ def detect(save_img=False):
                 for *xyxy, conf, cls in reversed(det):
                     if save_txt:  # Write to file
                         xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()  # normalized xywh
-                        line = (cls, *xywh, conf) if opt.save_conf else (cls, *xywh)  # label format
+                        line = (cls, *xywh, conf) if save_conf else (cls, *xywh)  # label format
                         with open(txt_path + '.txt', 'a') as f:
                             f.write(('%g ' * len(line)).rstrip() % line + '\n')
 
-                    if save_img or view_img:  # Add bbox to image
+                    if save_img:
                         label = f'{names[int(cls)]} {conf:.2f}'
                         plot_one_box(xyxy, im0, label=label, color=colors[int(cls)], line_thickness=1)
                     
@@ -257,7 +226,7 @@ def detect(save_img=False):
                 track_results['track_id'].append(tid)
 
                 #vertical = tlwh[2] / tlwh[3] > 1.6
-                #if tlwh[2] * tlwh[3] > opt.min_box_area and not vertical:
+                #if tlwh[2] * tlwh[3] > min_box_area and not vertical:
                 #    online_tlwhs.append(tlwh)
                 #    online_ids.append(tid)
                 #    online_scores.append(t.score)
@@ -268,12 +237,6 @@ def detect(save_img=False):
 
             # Print time (inference + NMS)
             print(f'{s}Done. ({(1E3 * (t2 - t1)):.1f}ms) Inference, ({(1E3 * (t3 - t2)):.1f}ms) NMS, ({t4-t3:.1f}ms) {len(online_targets)} Tracked.')
-
-            # Stream results
-            if view_img:
-                cv2.imshow(str(p), im0)
-                cv2.waitKey(1)  # 1 millisecond
-
             # Save results (image with detections)
             if save_img:
                 if dataset.mode == 'image':
@@ -293,11 +256,25 @@ def detect(save_img=False):
                             save_path += '.mp4'
                         vid_writer = cv2.VideoWriter(save_path, cv2.VideoWriter_fourcc(*'mp4v'), fps, (w, h))
                     vid_writer.write(im0)
+                    video_bucket_name, video_key = get_s3_bucket_and_key(output_video_location)
+                    s3.Bucket(video_bucket_name).upload_file(save_path, video_key)
 
+                    
     if save_txt or save_img:
         s = f"\n{len(list(save_dir.glob('labels/*.txt')))} labels saved to {save_dir / 'labels'}" if save_txt else ''
         print(f"Results saved to {save_dir}{s}")
         DataFrame(track_results).to_csv('{save_dir}tracked_data.csv')
-    
+        label_bucket_name, label_key = get_s3_bucket_and_key(output_label_location)
+        s3.Bucket(label_bucket_name).upload_file('{save_dir}tracked_data.csv', label_key)
+        
     print(f'Done. ({time.time() - t0:.3f}s)')
 
+
+# if __name__ == "__main__":
+#     model=model_fn("/home/ubuntu/yolo7-cctv-deployment-aws/temp")
+#     feed_data_dict={"s3_path":"s3://lightsketch-models-188775091215/models/VID_20200616_130248.mp4"}
+#     feed_data=json.dumps(feed_data_dict)
+#     transform_fn(model,feed_data,"application/json","")
+    
+    
+    
