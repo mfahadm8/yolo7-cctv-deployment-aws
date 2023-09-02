7743637cd78525c845877bcafd7a7ffd94286236
Ubuntu <ubuntu@ip-172-31-12-52.ec2.internal>
2023-09-02 19:26:54 +0000

    updated test event

--- infra/src/DbUpdateLambda/test.py
+++ infra/src/DbUpdateLambda/test.py
@@ -1,12 +1,4 @@
-import os
-
-os.environ["INPUT_BUCKET"]="sm-ball-tracking-inputs"
-os.environ["VIDEO_BUCKET"]="sm-ball-tracking-output-blobs"
-os.environ["SAGEMAKER_ENDPOINT_NAME"]="ball-tracking-v7"
-os.environ["LABELS_BUCKET"]="sm-ball-tracking-output-labels"
 
 from index import lambda_handler
-
-
-event={'input_s3_uri': 's3://sm-ball-tracking-input-blobs/20200616_VB_trim.mp4'} 
+event={"input_location": "s3://sm-ball-tracking-input-blobs/20200616_VB_trim.mp4", "output_label_location": "s3://sm-ball-tracking-output-labels/async-inference/e37255c7-eba6-4cc8-bff9-863c9b11212a/20200616_VB_trim.csv", "output_video_location": "s3://sm-ball-tracking-output-blobs/async-inference/e37255c7-eba6-4cc8-bff9-863c9b11212a/20200616_VB_trim.mp4", "inference_id": "e37255c7-eba6-4cc8-bff9-863c9b11212a"}
 lambda_handler(event,{})
