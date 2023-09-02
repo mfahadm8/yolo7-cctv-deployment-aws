bedec2047f4e1b3571d1eb3e689cfd38a89758d4
Ubuntu <ubuntu@ip-172-31-12-52.ec2.internal>
2023-09-02 21:13:51 +0000

    added mp4 and MOV filter

--- infra/src/NewVideoFileUploadTriggerLambda/index.py
+++ infra/src/NewVideoFileUploadTriggerLambda/index.py
@@ -10,20 +10,25 @@ def lambda_handler(events, context):
         for record in events["Records"]:
             bucket_name=urllib.parse.unquote(record["s3"]["bucket"]["name"])
             s3_file_path=urllib.parse.unquote(record["s3"]["object"]["key"])
-            s3_input_uri=f"s3://{bucket_name}/{s3_file_path}"
-            input_data = {
-                'input_s3_uri': s3_input_uri,
-            }
+            if s3_file_path.ends_with(".mp4") or s3_file_path.ends_with(".MOV"):
+                s3_input_uri=f"s3://{bucket_name}/{s3_file_path}"
+                input_data = {
+                    'input_s3_uri': s3_input_uri,
+                }
 
-            target_lambda_function_name = "SagemakerEndpointMediatorLambda"
+                target_lambda_function_name = "SagemakerEndpointMediatorLambda"
 
-            response = lambda_client.invoke(
-                FunctionName=target_lambda_function_name,
-                InvocationType='RequestResponse', 
-                Payload=json.dumps(input_data)  
-            )
-            
-            
+                response = lambda_client.invoke(
+                    FunctionName=target_lambda_function_name,
+                    InvocationType='RequestResponse', 
+                    Payload=json.dumps(input_data)  
+                )
+            else:
+                return {
+                    'statusCode': 200,
+                    'body': json.dumps('Not an .mp4 or .MOV file, ignoring it')
+                }
+                        
     return {
         'statusCode': 200,
         'body': json.dumps('Sagemaker Mediator Lambda executed successfully!')
