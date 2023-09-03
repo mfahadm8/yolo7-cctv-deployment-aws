7bbdd02fc0cee9901c6526d319db2958d08ab476
Ubuntu <ubuntu@ip-172-31-12-52.ec2.internal>
2023-09-03 19:37:11 +0000

    changed endpoint

--- test.py
+++ test.py
@@ -1,13 +1,13 @@
 import requests
 import json
 
-url = "https://5j6rdo6vke.execute-api.ca-central-1.amazonaws.com/prod"
+url = "https://vluhdpr7jc.execute-api.ca-central-1.amazonaws.com/prod"
 
 payload = {
     "input_s3_uri":"s3://test-vod-v120-source71e471f1-5vcytwlc3m1b/test-videos/20200616_VB_trim.mp4"
     }
 headers = {
-  'x-api-key': '0fzq66gtfR1vyrITumXTF6nH6Okmqzqx7mcomNwZ',
+  'x-api-key': 'fN8H3kB1Xu1ZwCTc2m6kJ9mEgWRUDtNW4yidjiSH',
   'Api-Pass': 'password',
   'Content-Type': 'text/plain'
 }
